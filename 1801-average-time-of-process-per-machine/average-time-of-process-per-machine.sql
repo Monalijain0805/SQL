586350f36a90964632bf67fe9a4d15ca8b92636a
Monali Jain <jainmonali0805@gmail.com>
2026-08-14 23:24:06 +0530

    Time: 350 ms (10.05%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -8,11 +8,13 @@
 
 -- half way to understand the problem
 
+
+
 -- actual ans -:
 
 -- SELECT 
 --     a1.machine_id,
---     ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time --  ROUND(AVG(a2.timestamp - a1.timestamp), 3)  this means we want round value upto 3 positions
+--     ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time            --  ROUND(AVG(a2.timestamp - a1.timestamp), 3)  this means we want round value upto 3 positions
 -- FROM Activity a1   -- start row
 -- INNER JOIN Activity a2  -- end row
 -- ON a1.process_id = a2.process_id  -- same process + 
@@ -33,3 +35,5 @@ AND a1.machine_id = a2.machine_id
 AND a1.activity_type = 'start'
 AND a2.activity_type = 'end'
 GROUP BY a1.machine_id;
+-- start/end → identify the correct rows
+-- timestamp → calculate the time difference
