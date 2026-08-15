289327a44b00fcbe9c7edd3a75a3e7aa7ce0a812
Monali Jain <jainmonali0805@gmail.com>
2026-08-15 19:34:01 +0530

    Time: 457 ms (14.69%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,20 +1,19 @@
-SELECT e1.name
--- We want to return the name of the manager
 
-FROM Employee e1
--- e1 represents the manager
 
-JOIN Employee e2
--- e2 represents the employees working under the manager
+-- Select the manager's name
+SELECT m.name
 
-ON e1.id = e2.managerId
--- Match manager's id with employee's managerId
--- This connects each employee to their manager
+-- e = employee table
+FROM Employee e
 
-GROUP BY e1.id, e1.name
--- Group all employees belonging to the same manager
--- So we can count how many direct reports each manager has
+-- m = same Employee table, but representing the manager
+JOIN Employee m
 
-HAVING COUNT(e2.id) >= 5;
--- Count the employees in each manager's group
+-- Connect each employee to their manager
+ON e.managerId = m.id
+
+-- Create one group for each manager
+GROUP BY m.id, m.name
+
 -- Keep only managers having 5 or more direct reports
+HAVING COUNT(e.id) >= 5;
